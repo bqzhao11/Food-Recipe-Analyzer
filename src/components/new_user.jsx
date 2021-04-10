f2fac32d3caf5ddf3f606143eb78eac261c29696
Ben Zhao <bqzhao11@gmail.com>
2021-04-10 14:53:05 -0500

    added new user page

---
+++ src/components/new_user.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+function NewUser() {
+    return(
+        <div>
+            <h2>This is the New User Page</h2>
+        </div>
+    );
+}
+
+export default NewUser;
+
