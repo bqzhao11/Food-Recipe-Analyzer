84c3d1aecb56f03d2e04d0544e1d3a6cc5adea83
Ben Zhao <bqzhao11@gmail.com>
2021-04-11 20:09:04 -0500

    push

--- backend/database.py
+++ backend/database.py
@@ -68,8 +68,6 @@ def delete_recipe(recipe_id):
     query = 'DELETE FROM Recipes WHERE recipeName = deleteRecipeName'
     conn.execute(query).fetchall()
     conn.close()
-<<<<<<< HEAD
-=======
 
 def add_user(user_data):
     conn = db.connect()
@@ -82,4 +80,3 @@ def add_user(user_data):
             f"'{user_data['location']}');"
     conn.execute(query)
     conn.close()
->>>>>>> master
